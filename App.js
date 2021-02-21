07da6202070e3d6fa0a6189c24223d7431a52c4e
Saad Surya <saadsurya@hotmail.com>
2021-02-21 20:56:13 +0500

    Integrated Firebase

--- App.js
+++ App.js
@@ -1,12 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
 
+import { StyleSheet, Text, TextInput, Touchable, TouchableOpacity, View } from 'react-native';
+import firebaseConfig from './FirebaseConfig';
+import firebase from 'firebase';
+
+var app = firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
+var objRef = database.ref('/obj');
 export default function App() {
+
+  useEffect(() => {
+    objRef.on('value', (value) => {
+      const data = value.val();
+      setVal(data.z);
+    });
+  }, []);
+
+  let changValueFunction = () => {
+    setVal("Changed Value!")
+  }
+  let changeToDB = () => {
+    objRef.set({
+      z: input
+    })
+  }
+
+  const [val, setVal] = useState("unknown");
+
+  const [input, setInput] = useState ("unknown1");
+
+  
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{val}</Text>
       <StatusBar style="auto" />
+      <TouchableOpacity onPress={changValueFunction}>Chang Value</TouchableOpacity>
+      <TextInput
+        style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+        onChangeText={text => setInput(text)}
+        value={input}
+      ></TextInput>
+      <TouchableOpacity onPress={changeToDB}>Change Value To DB</TouchableOpacity>
     </View>
   );
 }
@@ -19,3 +55,9 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+
+// Events (onPress in TouchableOpacity) (on Value change from firebase)
+// functions can be define with 3 different syntax.
+// useEffect with empty array - to execute a function once.
---
+++ FirebaseConfig.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBr8l8ZDsNwjD60yIUBcJN59S2Yc_SrDI8",
+    authDomain: "expo-9ad48.firebaseapp.com",
+    projectId: "expo-9ad48",
+    storageBucket: "expo-9ad48.appspot.com",
+    messagingSenderId: "525604838565",
+    appId: "1:525604838565:web:73fc798f0401308697c14c"
+  };
+
+export default firebaseConfig;
